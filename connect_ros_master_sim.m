fa60cd59a199f66b8801c3d4fc344658b52899ef
Masashi Kaneko <kmasash1995@gmail.com>
2017-04-28 21:24:53 +0900

    Rename

---
+++ connect_ros_master_sim.m
@@ -0,0 +1,70 @@
+%% Create a new ros master in MATLAB
+rosinit
+
+%% Open a MATLAB simulator (provided by MATLAB)
+% RobotSimulator is required to be in the path 
+sim = RobotSimulator('emptyMap')
+
+%% List all topics
+rostopic list
+
+%% Get more information about a topic
+rostopic info /odom
+
+% rostopic echo /odom
+% shows all messages of the topic
+
+%% Subscribe to topic
+odomSub = rossubscriber('/odom')
+
+%% Get infomation to see MATLAB as a subscriber(also a publisher) now
+rostopic info /odom
+
+%% Receive data using receive
+odomData = receive(odomSub)
+
+odomData = receive(odomSub,10) % wait max 10s
+
+odomData = odomSub.LatestMessage % get a latest message
+
+%% Use showdetails to show the contents of the message
+showdetails(odomData)
+
+%% Publish to topic
+% create a publisher
+velPub = rospublisher('/mobile_base/commands/velocity')
+
+%% Initialize the type and the content of data to publish
+velData = rosmessage(velPub);
+velData.Angular.Z = 10;
+velData.Linear.X = 1;
+
+%% Send data
+send(velPub,velData)
+
+%% Receive data
+odomData = odomSub.LatestMessage;
+
+%% Use showdetails() to see the content of data
+showdetails(odomData)
+
+%% List the existing services
+rosservice list
+
+%% Calling ROS services
+
+% reset the position of robot
+client = rossvcclient('/sim/reset_poses')
+call(client)
+
+% reset world
+resetWorld = rossvcclient('/gazebo/reset_world')
+call(resetWorld)
+
+%% Shut down ROS
+rosshutdown
+
+%% Clean up
+clear
+close
+clc
